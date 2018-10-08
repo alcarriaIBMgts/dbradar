509b47ad592ee34d527f2e8911dd5a57021b323a
Angel Alcarria <angel.alcarria.nieto1@ibm.com>
2018-10-08 10:58:47 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,505 @@
+
+
+(function () {
+  "use strict";
+
+  //set to true for debugging output
+  var debug = false;
+
+  //var utm = require('utm');
+
+
+  // our current position
+  var positionCurrent = {
+    lat: null,
+    lng: null,
+    hng: null
+  };
+
+  let balls = [
+    {ball:1,lat:40.44361383246247,lon:-3.6665857235848986,dist:null,x:null,y:null,recogida:false,px:null,py:null},
+    {ball:2,lat:null,lon:null,dist:null,x:null,y:null,recogida:true,px:null,py:null}
+  ];
+
+  var click = new Audio('clickradar.mp3');
+  click.play();
+  var shortbeep = new Audio('shortbeep2.mp3');
+  //audio.loop = true;
+
+  function fileToText(file, callback) {
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+      callback(reader.result);
+    };
+  }
+  var leido = 0;
+
+
+  // the outer part of the compass that rotates
+  var rose = document.getElementById("rose");
+  // Get the Object by ID
+	//var a = document.getElementById("svgObject");
+	// Get the SVG document inside the Object tag
+	//var svgDoc = rose.contentDocument;
+	// Get one of the SVG items by ID;
+	//var ball1 = svgDoc.getElementById("ball");
+	// Set the colour to something else
+
+  // elements that ouput our position
+  var positionLat = document.getElementById("position-lat");
+  var positionLng = document.getElementById("position-lng");
+  var positionHng = document.getElementById("position-hng");
+  var positionLatraw = document.getElementById("position-latraw");
+  var positionLngraw = document.getElementById("position-lngraw");
+  var ball1 = document.getElementById("ball1");
+  console.log(ball1);
+
+
+  // debug outputs
+  var debugOrientation = document.getElementById("debug-orientation");
+  var debugOrientationDefault = document.getElementById("debug-orientation-default");
+
+
+  // info popup elements, pus buttons that open popups
+  var popup = document.getElementById("popup");
+  var popupContents = document.getElementById("popup-contents");
+  var popupInners = document.querySelectorAll(".popup__inner");
+  //var btnsPopup = document.querySelectorAll(".btn-popup");
+
+
+  // buttons at the bottom of the screen
+  var btnLockOrientation = document.getElementById("btn-lock-orientation");
+  var btnNightmode = document.getElementById("btn-nightmode");
+  //var btnMap = document.getElementById("btn-map");
+  //var btnInfo = document.getElementById("btn-info");
+
+
+  // if we have shown the heading unavailable warning yet
+  var warningHeadingShown = false;
+
+
+  // switches keeping track of our current app state
+  var isOrientationLockable = false;
+  var isOrientationLocked = false;
+  var isNightMode = false;
+
+
+  // the orientation of the device on app load
+  var defaultOrientation;
+
+
+  // browser agnostic orientation
+  function getBrowserOrientation() {
+    var orientation;
+    if (screen.orientation && screen.orientation.type) {
+      orientation = screen.orientation.type;
+    } else {
+      orientation = screen.orientation ||
+                    screen.mozOrientation ||
+                    screen.msOrientation;
+    }
+
+    /*
+      'portait-primary':      for (screen width < screen height, e.g. phone, phablet, small tablet)
+                                device is in 'normal' orientation
+                              for (screen width > screen height, e.g. large tablet, laptop)
+                                device has been turned 90deg clockwise from normal
+
+      'portait-secondary':    for (screen width < screen height)
+                                device has been turned 180deg from normal
+                              for (screen width > screen height)
+                                device has been turned 90deg anti-clockwise (or 270deg clockwise) from normal
+
+      'landscape-primary':    for (screen width < screen height)
+                                device has been turned 90deg clockwise from normal
+                              for (screen width > screen height)
+                                device is in 'normal' orientation
+
+      'landscape-secondary':  for (screen width < screen height)
+                                device has been turned 90deg anti-clockwise (or 270deg clockwise) from normal
+                              for (screen width > screen height)
+                                device has been turned 180deg from normal
+    */
+
+    return orientation;
+  }
+
+
+  // browser agnostic orientation unlock
+  function browserUnlockOrientation() {
+    if (screen.orientation && screen.orientation.unlock) {
+      screen.orientation.unlock();
+    } else if (screen.unlockOrientation) {
+      screen.unlockOrientation();
+    } else if (screen.mozUnlockOrientation) {
+      screen.mozUnlockOrientation();
+    } else if (screen.msUnlockOrientation) {
+      screen.msUnlockOrientation();
+    }
+  }
+
+
+  // browser agnostic document.fullscreenElement
+  function getBrowserFullscreenElement() {
+    if (typeof document.fullscreenElement !== "undefined") {
+      return document.fullscreenElement;
+    } else if (typeof document.webkitFullscreenElement !== "undefined") {
+      return document.webkitFullscreenElement;
+    } else if (typeof document.mozFullScreenElement !== "undefined") {
+      return document.mozFullScreenElement;
+    } else if (typeof document.msFullscreenElement !== "undefined") {
+      return document.msFullscreenElement;
+    }
+  }
+
+
+  // browser agnostic document.documentElement.requestFullscreen
+  function browserRequestFullscreen() {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    }
+  }
+
+
+  // browser agnostic document.documentElement.exitFullscreen
+  function browserExitFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  }
+
+
+  // called on device orientation change
+  function onHeadingChange(event) {
+    var heading = event.alpha;
+
+    if (typeof event.webkitCompassHeading !== "undefined") {
+      heading = event.webkitCompassHeading; //iOS non-standard
+    }
+
+    var orientation = getBrowserOrientation();
+
+    if (typeof heading !== "undefined" && heading !== null) { // && typeof orientation !== "undefined") {
+      // we have a browser that reports device heading and orientation
+
+
+      if (debug) {
+        debugOrientation.textContent = orientation;
+      }
+
+
+      // what adjustment we have to add to rotation to allow for current device orientation
+      var adjustment = 0;
+      if (defaultOrientation === "landscape") {
+        adjustment -= 90;
+      }
+
+      if (typeof orientation !== "undefined") {
+        var currentOrientation = orientation.split("-");
+
+        if (defaultOrientation !== currentOrientation[0]) {
+          if (defaultOrientation === "landscape") {
+            adjustment -= 270;
+          } else {
+            adjustment -= 90;
+          }
+        }
+
+        if (currentOrientation[1] === "secondary") {
+          adjustment -= 180;
+        }
+      }
+
+      positionCurrent.hng = heading + adjustment;
+
+      var phase = positionCurrent.hng < 0 ? 360 + positionCurrent.hng : positionCurrent.hng;
+      positionHng.textContent = (360 - phase | 0) + "°";
+
+
+      // apply rotation to compass rose
+      if (typeof rose.style.transform !== "undefined") {
+        rose.style.transform = "rotateZ(-" + positionCurrent.hng + "deg)";
+      } else if (typeof rose.style.webkitTransform !== "undefined") {
+        rose.style.webkitTransform = "rotateZ(-" + positionCurrent.hng + "deg)";
+      }
+    } else {
+      // device can't show heading
+
+      positionHng.textContent = "n/a";
+      showHeadingWarning();
+    }
+  }
+
+  function showHeadingWarning() {
+    if (!warningHeadingShown) {
+      popupOpen("noorientation");
+      warningHeadingShown = true;
+    }
+  }
+
+  function onFullscreenChange() {
+    if (isOrientationLockable && getBrowserFullscreenElement()) {
+      if (screen.orientation && screen.orientation.lock) {
+        screen.orientation.lock(getBrowserOrientation()).then(function () {
+        }).catch(function () {
+        });
+      }
+    } else {
+      lockOrientationRequest(false);
+    }
+  }
+
+  function toggleOrientationLockable(lockable) {
+    isOrientationLockable = lockable;
+
+    if (isOrientationLockable) {
+      btnLockOrientation.classList.remove("btn--hide");
+
+      btnNightmode.classList.add("column-25");
+      btnNightmode.classList.remove("column-33");
+      //btnMap.classList.add("column-25");
+      //btnMap.classList.remove("column-33");
+      //btnInfo.classList.add("column-25");
+      //btnInfo.classList.remove("column-33");
+    } else {
+      btnLockOrientation.classList.add("btn--hide");
+
+      btnNightmode.classList.add("column-33");
+      btnNightmode.classList.remove("column-25");
+      //btnMap.classList.add("column-33");
+      //btnMap.classList.remove("column-25");
+      //btnInfo.classList.add("column-33");
+      //btnInfo.classList.remove("column-25");
+    }
+  }
+
+  function checkLockable() {
+    if (screen.orientation && screen.orientation.lock) {
+      screen.orientation.lock(getBrowserOrientation()).then(function () {
+        toggleOrientationLockable(true);
+        browserUnlockOrientation();
+      }).catch(function (event) {
+        if (event.code === 18) { // The page needs to be fullscreen in order to call lockOrientation(), but is lockable
+          toggleOrientationLockable(true);
+          browserUnlockOrientation(); //needed as chrome was locking orientation (even if not in fullscreen, bug??)
+        } else {  // lockOrientation() is not available on this device (or other error)
+          toggleOrientationLockable(false);
+        }
+      });
+    } else {
+      toggleOrientationLockable(false);
+    }
+  }
+
+  function lockOrientationRequest(doLock) {
+    if (isOrientationLockable) {
+      if (doLock) {
+        browserRequestFullscreen();
+        lockOrientation(true);
+      } else {
+        browserUnlockOrientation();
+        browserExitFullscreen();
+        lockOrientation(false);
+      }
+    }
+  }
+
+  function lockOrientation(locked) {
+    if (locked) {
+      btnLockOrientation.classList.add("active");
+    } else {
+      btnLockOrientation.classList.remove("active");
+    }
+
+    isOrientationLocked = locked;
+  }
+
+  function toggleOrientationLock() {
+    if (isOrientationLockable) {
+      lockOrientationRequest(!isOrientationLocked);
+    }
+  }
+
+  function locationUpdate(position) {
+    console.log(position);
+    positionCurrent.lat = position.coords.latitude;
+    positionCurrent.lng = position.coords.longitude;
+
+    positionLat.textContent = decimalToSexagesimal(positionCurrent.lat, "lat");
+    positionLng.textContent = decimalToSexagesimal(positionCurrent.lng, "lng");
+    positionLatraw.textContent = position.coords.accuracy;
+    positionLngraw.textContent = position.coords.speed;
+
+    //distance to ball
+
+    //latitud 90º es 10.000km mos o menos un cuadrante terrestre
+    //longitud 40.000 en 360º
+    //si lat1 - lat0 > 0 más arriba del origen
+    //si lon1 - lon0 > 0 más a la derecha del origen
+    for ( var i=0; i<balls.length;i++){
+      if (!balls[i].recogida) {
+        balls[i].x = (balls[i].lon-positionCurrent.lng) * Math.cos(balls[i].lat)*(40000/360);
+        balls[i].y = (balls[i].lat-positionCurrent.lat) * (10.000/90);
+        balls[i].dist = Math.sqrt(Math.pow(balls[i].x,2) + Math.pow(balls[i].y,2))*1000;
+        console.log(balls[i]);
+//3pixeles equivalen a 1m
+        if (balls[i].dist < 62 ) {
+          balls[i].px = 65 + balls[i].x*1000;
+          balls[i].py = 65 + balls[i].y*1000;
+          ball1.setAttribute("stroke", "darkorange");
+          ball1.setAttribute("fill", "orange");
+          ball1.setAttribute("r", "2");
+          shortbeep.loop=true;
+          shortbeep.play();
+        }
+        else{
+          ball1.setAttribute("stroke", "green");
+          ball1.setAttribute("fill", "green");
+          ball1.setAttribute("fill", "green");
+          ball1.setAttribute("r", "0");
+        }
+      };
+    }
+console.log(balls[0]);
+      ball1.setAttribute("cx", balls[0].px);
+      ball1.setAttribute("cy", balls[0].py);
+
+      fileToText("my-file.txt", (text) => {
+        console.log(text);
+  });
+  console.log(leido);
+
+  }
+
+  function locationUpdateFail(error) {
+    positionLat.textContent = "n/a";
+    positionLng.textContent = "n/a";
+    console.log("location fail: ", error);
+  }
+
+  function setNightmode(on) {
+
+    if (on) {
+      btnNightmode.classList.add("active");
+    } else {
+      btnNightmode.classList.remove("active");
+    }
+
+    window.setTimeout(function() {
+      if (on) {
+      //  document.documentElement.classList.add("nightmode");
+      } else {
+      //  document.documentElement.classList.remove("nightmode");
+      }
+    }, 1);
+
+
+    //isNightMode = on;
+  }
+
+  function toggleNightmode() {
+    //setNightmode(!isNightMode);
+  }
+
+  function openMap() {
+    //window.open("https://www.google.com/maps/place/@" + positionCurrent.lat + "," + positionCurrent.lng + ",16z", "_blank");
+  }
+  function playaudio() {
+    click.play();
+    shortbeep.play();
+  }
+  function popupOpenFromClick(event) {
+    //popupOpen(event.currentTarget.dataset.name);
+  }
+
+  function popupOpen(name) {
+    var i;
+    for (i=0; i<popupInners.length; i++) {
+    //  popupInners[i].classList.add("popup__inner--hide");
+    }
+    //document.getElementById("popup-inner-" + name).classList.remove("popup__inner--hide");
+
+  //  popup.classList.add("popup--show");
+  }
+
+  function popupClose() {
+  //  popup.classList.remove("popup--show");
+  }
+
+  function popupContentsClick(event) {
+  //  event.stopPropagation();
+  }
+
+  function decimalToSexagesimal(decimal, type) {
+    var degrees = decimal | 0;
+    var fraction = Math.abs(decimal - degrees);
+    var minutes = (fraction * 60) | 0;
+    var seconds = (fraction * 3600 - minutes * 60) | 0;
+
+    var direction = "";
+    var positive = degrees > 0;
+    degrees = Math.abs(degrees);
+    switch (type) {
+      case "lat":
+        direction = positive ? "N" : "S";
+        break;
+      case "lng":
+        direction = positive ? "E" : "W";
+        break;
+    }
+
+    return degrees + "° " + minutes + "' " + seconds + "\" " + direction;
+  }
+
+
+  if (screen.width > screen.height) {
+    defaultOrientation = "landscape";
+  } else {
+    defaultOrientation = "portrait";
+  }
+  if (debug) {
+    debugOrientationDefault.textContent = defaultOrientation;
+  }
+
+  window.addEventListener("deviceorientation", onHeadingChange);
+
+  document.addEventListener("fullscreenchange", onFullscreenChange);
+  document.addEventListener("webkitfullscreenchange", onFullscreenChange);
+  document.addEventListener("mozfullscreenchange", onFullscreenChange);
+  document.addEventListener("MSFullscreenChange", onFullscreenChange);
+
+  btnLockOrientation.addEventListener("click", toggleOrientationLock);
+  btnNightmode.addEventListener("click", playaudio);
+  //btnMap.addEventListener("click", playaudio);
+
+  //var i;
+  //for (i=0; i<btnsPopup.length; i++) {
+    //btnsPopup[i].addEventListener("click", popupOpenFromClick);
+  //}
+
+  //popup.addEventListener("click", popupClose);
+  //popupContents.addEventListener("click", popupContentsClick);
+
+  navigator.geolocation.watchPosition(locationUpdate, locationUpdateFail, {
+    enableHighAccuracy: true,
+    maximumAge: 0,
+    timeout: 2700
+  });
+
+  setNightmode(false);
+  checkLockable();
+
+}());
---
+++ server.js
@@ -0,0 +1,141 @@
+var express = require("express");
+var app = express();
+var cfenv = require("cfenv");
+var bodyParser = require('body-parser')
+var player = require('play-sound')(opts = {})
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+var cloudant, mydb;
+var utm = require('utm');
+
+var fs = require('fs');
+var data = fs.readFileSync('my-file.txt', 'utf8');
+console.log(data);
+if (data == 1){
+  console.log("Data is 1");
+}
+
+//player.play('radar.mp3', { timeout: 300 }, function(err){
+  //if (err) throw err
+//})
+
+
+/* Endpoint to greet and add a new visitor to database.
+* Send a POST request to localhost:3000/api/visitors with body
+* {
+* 	"name": "Bob"
+* }
+
+app.post("/api/visitors", function (request, response) {
+  var userName = request.body.name;
+  var doc = { "name" : userName };
+  if(!mydb) {
+    console.log("No database.");
+    response.send(doc);
+    return;
+  }
+  // insert the username as a document
+  mydb.insert(doc, function(err, body, header) {
+    if (err) {
+      console.log('[mydb.insert] ', err.message);
+      response.send("Error");
+      return;
+    }
+    doc._id = body.id;
+    response.send(doc);
+  });
+});
+
+/**
+ * Endpoint to get a JSON array of all the visitors in the database
+ * REST API example:
+ * <code>
+ * GET http://localhost:3000/api/visitors
+ * </code>
+ *
+ * Response:
+ * [ "Bob", "Jane" ]
+ * @return An array of all the visitor names
+*/
+app.get("/1", function (request, response) {
+data = 1;
+fs.writeFile("my-file.txt", "1", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved with 1!");
+});
+return 1;
+});
+
+app.get("/2", function (req, res) {
+  data = 2;
+  fs.writeFile("my-file.txt", "2", function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("The file was saved with 2!");
+      res.sendFile(__dirname + '/dragon.html')});
+
+  });
+
+/*
+// load local VCAP configuration  and service credentials
+var vcapLocal;
+try {
+  vcapLocal = require('./vcap-local.json');
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
+// Load the Cloudant library.
+var Cloudant = require('@cloudant/cloudant');
+if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
+
+  // Initialize database with credentials
+  if (appEnv.services['cloudantNoSQLDB']) {
+    // CF service named 'cloudantNoSQLDB'
+    cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+  } else {
+     // user-provided service with 'cloudant' in its name
+     cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);
+  }
+} else if (process.env.CLOUDANT_URL){
+  cloudant = Cloudant(process.env.CLOUDANT_URL);
+}
+if(cloudant) {
+  //database name
+  var dbName = 'mydb';
+
+  // Create a new "mydb" database.
+  cloudant.db.create(dbName, function(err, data) {
+    if(!err) //err if database doesn't already exists
+      console.log("Created database: " + dbName);
+  });
+
+  // Specify the database we are going to use (mydb)...
+  mydb = cloudant.db.use(dbName);
+}
+*/
+//serve static file (index.html, images, css)
+//if (fs.readFileSync('my-file.txt', 'utf8')==1)
+//app.use(express.static(__dirname));
+//else
+app.use(express.static(__dirname));
+
+
+
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
